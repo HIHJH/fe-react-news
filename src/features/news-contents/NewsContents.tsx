6ec89fcfe6e3e6f708c9fa173661813ad022d451
HIHJH <2170095@ewhain.net>
2026-01-14 18:32:55 +0900

    fix: 탭 상태 useReducer -> useState 로 관리하도록 수정

--- src/features/news-contents/NewsContents.tsx
+++ src/features/news-contents/NewsContents.tsx
@@ -1,29 +1,34 @@
 import NewsTab from "@/features/news-contents/components/NewsTab";
 import GridContainer from "@/features/news-contents/components/grid/GridContainer";
 import List from "@/features/news-contents/components/list/List";
-import { useReducer } from "react";
-import {
-  newsReducer,
-  initialState,
-} from "@/features/news-contents/store/newsReducer";
-import { useFilterHandlers } from "@/features/news-contents/hooks/useFilterHandlers";
+import { useState } from "react";
+
+export type NewsViewMode =
+  | "GRID_ALL"
+  | "GRID_SUBSCRIBED"
+  | "LIST_ALL"
+  | "LIST_SUBSCRIBED";
+
+export type NewsState = {
+  viewMode: NewsViewMode;
+};
 
 const NewsContents = () => {
-  const [newsState, dispatch] = useReducer(newsReducer, initialState);
-  const { handleNewsFilter, handleNewsViewMode } = useFilterHandlers(dispatch);
+  const [viewMode, setViewMode] = useState<NewsViewMode>("GRID_ALL");
 
-  const isGrid = newsState.viewMode === "grid";
-  const isFiltered = newsState.filter === "subscribed";
+  const isGrid = viewMode === "GRID_ALL" || viewMode === "GRID_SUBSCRIBED";
+  const isFiltered =
+    viewMode === "GRID_SUBSCRIBED" || viewMode === "LIST_SUBSCRIBED";
+
   const ContentsComponent = isGrid ? GridContainer : List;
+
+  const handleViewModeChange = (newMode: NewsViewMode) => {
+    setViewMode(newMode);
+  };
 
   return (
     <section>
-      <NewsTab
-        isGrid={isGrid}
-        isFiltered={isFiltered}
-        onFilterChange={handleNewsFilter}
-        onViewChange={handleNewsViewMode}
-      />
+      <NewsTab viewMode={viewMode} onViewModeChange={handleViewModeChange} />
       <ContentsComponent isFiltered={isFiltered} />
     </section>
   );
--- src/features/news-contents/components/NewsTab.tsx
+++ src/features/news-contents/components/NewsTab.tsx
@@ -1,48 +1,69 @@
 import GridIcon from "@/assets/icons/grid_icon.svg?react";
 import ListIcon from "@/assets/icons/list_icon.svg?react";
+import type { NewsViewMode } from "@/features/news-contents/NewsContents";
 
 interface NewsTabProps {
-  isGrid: boolean;
-  isFiltered: boolean;
-  onFilterChange: (filter: "all" | "subscribed") => void;
-  onViewChange: (view: "grid" | "list") => void;
+  viewMode: NewsViewMode;
+  onViewModeChange: (viewMode: NewsViewMode) => void;
 }
 
-const NewsTab = ({
-  isGrid,
-  isFiltered,
-  onFilterChange,
-  onViewChange,
-}: NewsTabProps) => {
+const NewsTab = ({ viewMode, onViewModeChange }: NewsTabProps) => {
+  const isGrid = viewMode === "GRID_ALL" || viewMode === "GRID_SUBSCRIBED";
+  const isFiltered =
+    viewMode === "GRID_SUBSCRIBED" || viewMode === "LIST_SUBSCRIBED";
+
+  const handleFilterChange = (filter: "all" | "subscribed") => {
+    const newViewMode =
+      filter === "all"
+        ? isGrid
+          ? "GRID_ALL"
+          : "LIST_ALL"
+        : isGrid
+        ? "GRID_SUBSCRIBED"
+        : "LIST_SUBSCRIBED";
+    onViewModeChange(newViewMode);
+  };
+
+  const handleViewChange = (view: "grid" | "list") => {
+    const newViewMode =
+      view === "grid"
+        ? isFiltered
+          ? "GRID_SUBSCRIBED"
+          : "GRID_ALL"
+        : isFiltered
+        ? "LIST_SUBSCRIBED"
+        : "LIST_ALL";
+    onViewModeChange(newViewMode);
+  };
+
   return (
     <div className="flex justify-between">
       <div className="flex gap-6">
         <TabButton
           isActive={!isFiltered}
           buttonText="전체 언론사"
-          onClick={() => onFilterChange("all")}
+          onClick={() => handleFilterChange("all")}
         />
         <TabButton
           isActive={isFiltered}
           buttonText="내가 구독한 언론사"
-          onClick={() => onFilterChange("subscribed")}
+          onClick={() => handleFilterChange("subscribed")}
         />
       </div>
       <div className="flex gap-2">
         <ListIcon
           className={isGrid ? "text-weak" : "text-point"}
-          onClick={() => onViewChange("list")}
+          onClick={() => handleViewChange("list")}
         />
         <GridIcon
           className={isGrid ? "text-point" : "text-weak"}
-          onClick={() => onViewChange("grid")}
+          onClick={() => handleViewChange("grid")}
         />
       </div>
     </div>
   );
 };
 
-// 수정 예정
 interface TabButtonProps {
   isActive?: boolean;
   buttonText: string;
